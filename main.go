924ad764344d51d4efab063a9156e86ab33036e2
autom8ter <colemanword@gmail.com>
2020-10-26 15:39:02 -0600

    import/export

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/autom8ter/dagger"
 	"github.com/autom8ter/graphik/graph"
 	"github.com/autom8ter/graphik/graph/generated"
 	"github.com/autom8ter/graphik/logger"
@@ -12,7 +13,6 @@ import (
 	"github.com/prometheus/client_golang/prometheus/promhttp"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
-	"log"
 	"net"
 	"net/http"
 	"net/http/pprof"
@@ -27,6 +27,7 @@ const version = "0.0.0"
 func init() {
 	viper.SetConfigFile("graphik.yaml")
 	viper.SetDefault("server.port", 8080)
+	viper.SetDefault("database.path", "/tmp/graphik/graphik.db")
 	viper.AutomaticEnv()
 	viper.ReadInConfig()
 }
@@ -38,6 +39,18 @@ func main() {
 	interrupt := make(chan os.Signal, 1)
 	signal.Notify(interrupt, os.Interrupt, syscall.SIGTERM)
 	defer signal.Stop(interrupt)
+
+	{
+		f, err := getDBFile()
+		if err != nil {
+			logger.Error("failed to open database", zap.Error(err))
+			return
+		}
+		if err := dagger.ImportJSON(f); err != nil {
+			logger.Error("failed to import database", zap.Error(err))
+		}
+		logger.Info("node types", zap.Strings("types", dagger.NodeTypes()))
+	}
 
 	mach := machine.New(ctx)
 	mux := http.NewServeMux()
@@ -59,7 +72,7 @@ func main() {
 
 		lis, err := net.Listen("tcp", fmt.Sprintf(":%v", port))
 		if err != nil {
-			log.Printf("failed to create server listener: %s", err)
+			logger.Error("failed to create server listener", zap.Error(err))
 			return
 		}
 		defer lis.Close()
@@ -79,11 +92,30 @@ func main() {
 		mach.Cancel()
 		break
 	}
-	logger.Info("shutdown signal received")
+	logger.Warn("shutdown signal received")
 	shutdownCtx, shutdownCancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer shutdownCancel()
 
 	_ = server.Shutdown(shutdownCtx)
+
+	{
+		f, err := getDBFile()
+		if err != nil {
+			logger.Error("failed to open database", zap.Error(err))
+		}
+		if err := f.Truncate(0); err != nil {
+			logger.Error("failed to export graph", zap.Error(err))
+		}
+		if err := dagger.ExportJSON(f); err != nil {
+			logger.Error("failed to export graph", zap.Error(err))
+		}
+	}
 	logger.Info("shutdown successful")
 	mach.Wait()
+}
+
+
+func getDBFile() (*os.File, error) {
+	os.MkdirAll("/tmp/graphik/", os.ModePerm)
+	return os.OpenFile(viper.GetString("database.path"), os.O_CREATE|os.O_RDWR, os.ModePerm)
 }
