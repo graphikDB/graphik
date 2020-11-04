732cd32a3023bf33d77265fb94710325102eaad9
autom8ter <colemanword@gmail.com>
2020-11-04 10:06:19 -0700

    admin/user

--- api/admin/resolver/schema.resolvers.go
+++ api/admin/resolver/schema.resolvers.go
@@ -234,13 +234,3 @@ func (r *Resolver) Subscription() generated1.SubscriptionResolver { return &subs
 type mutationResolver struct{ *Resolver }
 type queryResolver struct{ *Resolver }
 type subscriptionResolver struct{ *Resolver }
-
-// !!! WARNING !!!
-// The code below was going to be deleted when updating resolvers. It has been copied here so you have
-// one last chance to move it out of harms way if you want. There are two reasons this happens:
-//  - When renaming or deleting a resolver the old code will be put in here. You can safely delete
-//    it when you're done.
-//  - You have helper methods in this file. Move them out to keep these resolver files clean.
-func (r *mutationResolver) CreateNodes(ctx context.Context, input model.NodeConstructor) (*model.Node, error) {
-	panic(fmt.Errorf("not implemented"))
-}
