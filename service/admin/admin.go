b30b03d2c81f5aa44c1ffd5781e61afff2fc1127
autom8ter <colemanword@gmail.com>
2020-11-04 22:13:56 -0700

    progress

--- service/admin/admin.go
+++ service/admin/admin.go
@@ -14,14 +14,26 @@ func NewService(runtime *runtime.Runtime) *Service {
 	return &Service{runtime: runtime}
 }
 
-func (s Service) RaftJoin(ctx context.Context, request *apipb.RaftJoinRequest) (*apipb.RaftJoinResponse, error) {
+func (s *Service) implements() apipb.AdminServiceServer {
+	return s
+}
+
+func (s *Service) RaftJoin(ctx context.Context, request *apipb.RaftJoinRequest) (*apipb.RaftJoinResponse, error) {
 	panic("implement me")
 }
 
-func (s Service) GetJWKS(ctx context.Context, request *apipb.GetJWKSRequest) (*apipb.GetJWKSResponse, error) {
+func (s *Service) GetJWKS(ctx context.Context, request *apipb.GetJWKSRequest) (*apipb.GetJWKSResponse, error) {
 	panic("implement me")
 }
 
-func (s Service) UpdateJWKS(ctx context.Context, request *apipb.UpdateJWKSRequest) (*apipb.UpdateJWKSResponse, error) {
+func (s *Service) UpdateJWKS(ctx context.Context, request *apipb.UpdateJWKSRequest) (*apipb.UpdateJWKSResponse, error) {
+	panic("implement me")
+}
+
+func (s *Service) SearchNodes(ctx context.Context, request *apipb.SearchNodesRequest) (*apipb.SearchNodesResponse, error) {
+	panic("implement me")
+}
+
+func (s *Service) SearchEdges(ctx context.Context, request *apipb.SearchEdgesRequest) (*apipb.SearchEdgesResponse, error) {
 	panic("implement me")
 }
