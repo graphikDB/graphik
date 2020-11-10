78850cfc87add63322d159c0a141e4b6e30aa50f
autom8ter <colemanword@gmail.com>
2020-11-10 14:29:52 -0700

    better test

--- main_test.go
+++ main_test.go
@@ -15,9 +15,6 @@ import (
 
 func init() {
 	godotenv.Load()
-}
-
-func Test(t *testing.T) {
 	cfg := &apipb.Config{
 		Auth: &apipb.AuthConfig{
 			JwksSources: []string{"https://www.googleapis.com/oauth2/v3/certs"},
@@ -26,11 +23,15 @@ func Test(t *testing.T) {
 	cfg.SetDefaults()
 	ctx, cancel := context.WithTimeout(context.Background(), 15*time.Second)
 	go func() {
-
 		defer cancel()
 		run(ctx, cfg)
 	}()
+}
+
+func Test(t *testing.T) {
 	time.Sleep(5 * time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
 	j, err := google.DefaultTokenSource(ctx, "https://www.googleapis.com/auth/devstorage.full_control")
 	if err != nil {
 		t.Fatal(err)
@@ -47,14 +48,29 @@ func Test(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	client := apipb.NewConfigServiceClient(conn)
-	pong, err := client.Ping(ctx, &empty.Empty{})
+	var (
+		cfgClient = apipb.NewConfigServiceClient(conn)
+		gClient   = apipb.NewGraphServiceClient(conn)
+	)
+	pong, err := cfgClient.Ping(ctx, &empty.Empty{})
 	if err != nil {
 		t.Fatal(err)
 	}
 	if pong.Message != "PONG" {
 		t.Fatal("not PONG")
 	}
+	nodes, err := gClient.SearchNodes(ctx, &apipb.TypeFilter{
+		Type:        "identity",
+		Expressions: nil,
+		Limit:       1,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	for _, n := range nodes.GetNodes() {
+		t.Log(n.String())
+	}
+
 	select {
 	case <-ctx.Done():
 		t.Log("done")
