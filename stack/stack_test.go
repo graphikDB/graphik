51e01d5886af4fe7c98b772f13189ed2ac809fa9
autom8ter <colemanword@gmail.com>
2020-12-01 16:54:46 -0700

    stack test

---
+++ stack/stack_test.go
@@ -0,0 +1,18 @@
+package stack_test
+
+import (
+	stack2 "github.com/autom8ter/graphik/stack"
+	"testing"
+)
+
+func Test(t *testing.T) {
+	stck := stack2.New()
+	stck.Push("hello")
+	if stck.Len() != 1 {
+		t.Fatal("expected length of one")
+	}
+	val := stck.Pop()
+	if val.(string) != "hello" {
+		t.Fatal("expected hello")
+	}
+}
